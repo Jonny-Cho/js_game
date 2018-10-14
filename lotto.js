0df733597367fba8b747ffcf029aa9f4ac4d6a26
unknown <andante2183@gmail.com>
2018-10-14 21:59:02 +0900

    Finish lotto app ver0.1

--- lotto.js
+++ lotto.js
@@ -21,17 +21,59 @@ var bonusNum = shuffle[shuffle.length - 1];
 var luckyNum = shuffle.slice(0, 6);
 console.log('당첨숫자', luckyNum.sort((p,c)=>(p-c)), '보너스', bonusNum);
 
-// 당첨숫자를 당첨공(HTML)에 담기
-var resultDiv = document.getElementById('result');
-for(var i=0 ; i<luckyNum.length; i++){
-    var luckyBall = document.createElement('div');
-    luckyBall.textContent = luckyNum[i];
-    resultDiv.appendChild(luckyBall);
+// 당첨숫자를 당첨공(HTML)에 담기 1초에 하나씩
+var resultDiv = document.querySelector('#result');
+
+// for(var i=0 ; i<luckyNum.length; i++){
+//     setTimeout(function callback(){
+//         var luckyBall = document.createElement('div');
+//         luckyBall.textContent = luckyNum[i];
+//         resultDiv.appendChild(luckyBall);
+//     }, 1000);
+// }
+
+// 비동기함수 클로저문제 해결
+for(var i=0; i<luckyNum.length; i++){
+    (function(j){
+        setTimeout(function callback(){
+            var luckyBall = document.createElement('div');
+            luckyBall.textContent = luckyNum[j];
+            var bg;
+            if(luckyNum[j]<=10){
+                bg = 'red';
+            } else if (luckyNum[j]<=20) {
+                bg = 'orange';
+            } else if (luckyNum[j]<=30) {
+                bg = 'yellow';
+            } else if (luckyNum[j]<=40) {
+                bg = 'blue';
+            } else {
+                bg = 'green';
+            }
+            luckyBall.style.background = bg;
+            resultDiv.appendChild(luckyBall);
+        }, j*1000);
+    })(i);
 }
 
 // 보너스 숫자를 보너스공(HTML)에 담기
-var bonusDiv = document.getElementsByClassName('bonus')[0];
-var bonusBall = document.createElement('div');
 
-bonusBall.textContent = bonusNum;
-bonusDiv.appendChild(bonusBall);
+setTimeout(function(){
+    var bonusDiv = document.querySelector('.bonus')[0];
+    var bonusBall = document.createElement('div');
+    bonusBall.textContent = bonusNum;
+    var bg;
+    if(bonusNum<=10){
+        bg = 'red';
+    } else if (bonusNum<=20) {
+        bg = 'orange';
+    } else if (bonusNum<=30) {
+        bg = 'yellow';
+    } else if (bonusNum<=40) {
+        bg = 'blue';
+    } else {
+        bg = 'green';
+    }
+    bonusBall.style.background = bg;
+    bonusDiv.appendChild(bonusBall);
+}, 6000);
